0a951892e4ef4cd19bfc41245a74c8b337301910
AmirHossein Mahmoodi <amir.mahmoodi74@gmail.com>
2024-01-05 23:59:43 +0330

    fixed bug

--- next.config.js
+++ next.config.js
@@ -18,8 +18,6 @@ module.exports = (phase) => {
     if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
         const withPWA = require("@ducanh2912/next-pwa").default({
             dest: "public",
-            cacheOnFrontendNav: true,
-            cacheStartUrl: true
         });
         return withPWA(nextConfig);
     }
