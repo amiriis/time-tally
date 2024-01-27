385cdc5d9ae807f0fcbaae94e86861b1a7abe730
AmirHossein Mahmoodi <amir.mahmoodi74@gmail.com>
2024-01-27 14:51:58 +0330

    fixed bug icome

--- src/components/IcomeWork/index.jsx
+++ src/components/IcomeWork/index.jsx
@@ -3,14 +3,19 @@ import React from "react";
 import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 
 function IncomeWork({ durationInMilliseconds, times, work }) {
-  const total_hours = durationInMilliseconds / 60 / 60 / 1000;
+  const total_hours = Math.round(durationInMilliseconds / 1000 / 60) / 60;
   const income = total_hours * work.settings.income_coefficient;
+
+  console.log(
+    (Math.round(durationInMilliseconds / 1000 / 60) / 60) *
+      work.settings.income_coefficient
+  );
 
   return (
     <Stack direction={"row"} sx={{ px: 2 }} justifyContent={"space-between"}>
       <Typography>Income</Typography>
       {times ? (
-        <Stack direction={"row"} spacing={1} alignItems={'center'}>
+        <Stack direction={"row"} spacing={1} alignItems={"center"}>
           <PointOfSaleIcon fontSize="small" sx={{ color: "primary.main" }} />
           <Typography color="primary.main">
             {Math.round(income).toLocaleString("en")}
