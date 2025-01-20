e5498ec4d66fd79ea7bbb8c497d2795171645784
AmirHossein Mahmoodi <amir.mahmoodi74@gmail.com>
2025-01-20 17:34:51 +0330

    show list for current month

--- src/components/ListTime/index.jsx
+++ src/components/ListTime/index.jsx
@@ -1,27 +1,39 @@
-import { Collapse, Stack } from "@mui/material";
-import TimeCard from "./TimeCard";
-import { TransitionGroup } from "react-transition-group";
 import { db } from "@/lib/firebase";
+import { Collapse, Stack } from "@mui/material";
 import {
   collection,
-  getDocs,
+  onSnapshot,
   orderBy,
   query,
-  where,
-  onSnapshot,
+  where
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { TransitionGroup } from "react-transition-group";
 import NotFoundData from "../NotFoundData";
+import TimeCard from "./TimeCard";
 
 function ListTime({ work }) {
   const [listTime, setListTime] = useState();
 
   useEffect(() => {
+    const getCurrentShamsiMonthRange = () => {
+      const moment = require("jalali-moment");
+      const now = moment();
+      const startOfMonth = now.startOf("jMonth").toDate();
+      const endOfMonth = now.endOf("jMonth").toDate();
+      return { startOfMonth, endOfMonth };
+    };
+
+    const { startOfMonth, endOfMonth } = getCurrentShamsiMonthRange();
+
     const q = query(
       collection(db, "times"),
       where("wid", "==", work.id),
+      where("created_at", ">=", startOfMonth),
+      where("created_at", "<=", endOfMonth),
       orderBy("created_at", "desc")
     );
+
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const _times = [];
       querySnapshot.forEach((doc) => {
